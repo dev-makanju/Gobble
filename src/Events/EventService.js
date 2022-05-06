b3d952eb765da1d874713685ac758ecc093d621d
miracool <makurseme@gmail.com>
2022-05-06 17:46:54 +0100

    completed all endpoint

--- src/Events/EventService.js
+++ src/Events/EventService.js
@@ -5,9 +5,9 @@ const token = localStorage.getItem('gobtoken');
 const apiClient = axios.create({
     baseURL:'https://gobble-foods.herokuapp.com/api/v1/',
     headers:{
-       'Access-Control-Allow-Origin': '*',
-       'Authorization': 'Bearer'+token,
-       'Content-Type': 'application/json'
+      'Access-Control-Allow-Origin': '*',
+      'Authorization': 'Bearer'+token,
+      'Content-Type': 'application/json'
     }
 });
 
@@ -23,13 +23,53 @@ export default {
    passwordResetEvent(data){
       return apiClient.post('forgotPassword' , data);
    },
+   getUserEvent(){
+      return apiClient.get('me')
+   },
+   getUserByIdEvent(id){
+      return apiClient.get('user/'+id)
+   },
+   updatePasswordEvent(data){
+      return apiClient.post('user/updatePassword' , data )
+   },
+
+   //delete your account
+   deleteUserEvent(){
+      return apiClient.delete('user/deleteAccount')
+   },
+   makeUserAdminEvent(data){
+      return apiClient.get('user/makeAdmin' , data)//accepting email
+   },
+   getAllUserEvent(){
+      return apiClient.get('users/?page=1&limit=10');
+   },
+   editUserProfileEvent(data){
+      return apiClient.post('user/editProfile' , data);
+   },   
 
    //product handler services
    getProductEvent(){
       return apiClient.get('products')
    },
+   //write review
+   reviewEvent(productId){
+      return apiClient.post(`reviews/${productId}`);
+   },
+   getProductReviewsEvent(productId){
+      return apiClient.post('reviews/'+productId)
+   },
+   // Cart  System
+   getUserCartEvent(){
+      return apiClient.get('mycart');
+   },
+   deleteCartItem(productId){
+      return apiClient.delete('products/cart/'+productId);
+   },
    createProductEvent(data){
       return apiClient.post(`products` , data)
+   },
+   productQuantityEvent(data){
+      return apiClient.post(`products/cart/${data.id}`, data.qty )
    },
    getProductByIdEvent(data){
       return apiClient.get(`products/${data.id}`)
@@ -39,8 +79,10 @@ export default {
    },
    deleteProductEvent(data){
       return apiClient.delete(`products/${data.id}`)
-   }
-
+   },
    //search for a single product
+   getProductBySearchEvent(data){
+      return apiClient.get('queryProducts' , data)
+   },
  
 }
